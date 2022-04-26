09deed58434f84e66932f1dfbc1401e167d93682
Sumit <sumitj00112233@gmail.com>
2022-04-26 19:45:48 +0530

    90_DAYS_OF_CODE

---
+++ Array.java
@@ -0,0 +1,50 @@
+class Array{
+ static void reverseArray(int[] arr , int start , int end){
+   int  temp;
+
+ while(start<end){
+     temp=arr[start];
+     arr[start]=arr[end];
+     arr[end]=temp;
+     start++;
+     end--;
+ }
+ }
+
+ static void printArray(int[] arr , int size){
+for(int i = 0; i<size;i++){
+    System.out.print(arr[i]+" ");
+   
+}
+System.out.println();
+
+ }
+
+ public static void main(String[] args){
+     int[] arr={4,5,6,7,8,9};
+     printArray(arr, 5);
+     reverseArray(arr, 0, 4);
+     System.out.print("After reversing a array..\n");
+    printArray(arr, 5);
+
+    //diff APPROACH 
+    System.out.println("Original Array");
+     int[] arr2 ={1,2,3,4,5};
+    
+     for(int i=0;i<arr2.length;i++){
+        System.out.print(arr2[i]+" ");
+     }
+     System.out.println();
+     System.out.println("After reversing..");
+     for(int i= arr2.length-1;i>=0;i--){
+
+         System.out.print(arr2[i]+" ");
+     }
+    
+
+
+ }
+
+
+
+}
